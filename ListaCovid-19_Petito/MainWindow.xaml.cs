8108dd0819fd039e09e4d07e0d53d3c035c3c7bf
gianluca pet <gian.arma@live.it>
2020-04-17 19:27:57 +0200

    Aggiungere i file di progetto.

---
+++ ListaCovid-19_Petito/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using ListaCovid_19_Petito.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace ListaCovid_19_Petito
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        List<DatoCovid> ListaCovid = new List<DatoCovid>();
+
+        private void Btn_Covid_19_Click(object sender, RoutedEventArgs e)
+        {
+            Lstl_Covid_19.Items.Clear();
+
+            Task.Factory.StartNew(() =>
+            {
+                Carica();
+            });
+        }
+
+        public void Carica()
+        {
+
+            string file = @"DatiCovid.xml";
+            XDocument xmlDoc = XDocument.Load(file);
+            XElement xmlroot = xmlDoc.Element("root");
+            var xmlrow = xmlroot.Elements("row");
+
+            foreach (var item in xmlrow)
+            {
+                Thread.Sleep(1000);
+                XElement xmlData = item.Element("data");
+                XElement xmlStato = item.Element("stato");
+                XElement xmricoverati_con_sintomi = item.Element("ricoverati_con_sintomi");
+                XElement xmlterapia_intensiva = item.Element("terapia_intensiva");
+                XElement xmltotale_ospedalizzati = item.Element("totale_ospedalizzati");
+                XElement xmlisolamento_domiciliare = item.Element("isolamento_domiciliare");
+                XElement xmltotale_positivi = item.Element("totale_positivi");
+                XElement xmlvariazione_totale_positivi = item.Element("variazione_totale_positivi");
+                XElement xmlnuovi_positivi = item.Element("nuovi_positivi");
+                XElement xmldimessi_guariti = item.Element("dimessi_guariti");
+                XElement xmldeceduti = item.Element("deceduti");
+                XElement xmltotale_casi = item.Element("totale_casi");
+                XElement xmltamponi = item.Element("tamponi");
+                XElement xmlnote_it = item.Element("note_it");
+                XElement xmlnote_en = item.Element("note_en");
+
+                DatoCovid a = new DatoCovid();
+                a.data = Convert.ToDateTime(xmlData.Value);
+                a.stato = xmlStato.Value;
+                a.ricoverati_con_sintomi = Convert.ToInt32(xmricoverati_con_sintomi.Value);
+                a.terapia_intensiva = Convert.ToInt32(xmlterapia_intensiva.Value);
+                a.totale_ospedalizzati = Convert.ToInt32(xmltotale_ospedalizzati.Value);
+                a.isolamento_domiciliare = Convert.ToInt32(xmlisolamento_domiciliare.Value);
+                a.totale_positivi = Convert.ToInt32(xmltotale_positivi.Value);
+                a.variazione_totale_positivi = Convert.ToInt32(xmlvariazione_totale_positivi.Value);
+                a.nuovi_positivi = Convert.ToInt32(xmlnuovi_positivi.Value);
+                a.dimessi_guariti = Convert.ToInt32(xmldimessi_guariti.Value);
+                a.deceduti = Convert.ToInt32(xmldeceduti.Value);
+                a.totale_casi = Convert.ToInt32(xmltotale_casi.Value);
+                a.tamponi = Convert.ToInt32(xmltamponi.Value);
+                a.note_it = xmlnote_it.Value;
+                a.note_en = xmlnote_en.Value;
+                Dispatcher.Invoke(() =>
+                {
+                    Lstl_Covid_19.Items.Add(a);
+                });
+
+            }
+        }
+    }
+}
---
+++ ListaCovid-19_Petito/Model/DatoCovid.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaCovid_19_Petito.Model
+{
+    class DatoCovid
+    {
+        public DateTime data { get; set; }
+        public string stato { get; set; }
+        public int ricoverati_con_sintomi { get; set; }
+        public int terapia_intensiva { get; set; }
+        public int totale_ospedalizzati { get; set; }
+        public int isolamento_domiciliare { get; set; }
+        public int totale_positivi { get; set; }
+        public int variazione_totale_positivi { get; set; }
+        public int nuovi_positivi { get; set; }
+        public int dimessi_guariti { get; set; }
+        public int deceduti { get; set; }
+        public int totale_casi { get; set; }
+        public int tamponi { get; set; }
+        public string note_it { get; set; }
+        public string note_en { get; set; }
+
+        public override string ToString()
+        {
+            return $"Stato: {stato} Data: {data.ToShortDateString()} nuovi positivi: {nuovi_positivi} totale posivi: {totale_positivi} ricoverati: {ricoverati_con_sintomi} deceduti: {deceduti}";
+        }
+    }
+}
